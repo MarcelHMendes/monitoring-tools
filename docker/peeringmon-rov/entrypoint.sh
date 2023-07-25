0cb28ec431d26c6eb4cbd61871bbf93880932d45
Marcel Mendes <marcelmendes@dcc.ufmg.br>
2023-07-25 15:09:07 -0300

    add env file

--- docker/peeringmon-rov/entrypoint.sh
+++ docker/peeringmon-rov/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 pushd /root/
 exec python3 bgpstream-downloader.py --prefixes "138.185.229.0/24" --dump_type "updates" --project "ripe_ris" --start-date "2023-07-18" --stop-date "2023-07-23"
-#exec python3 fetch-measurements.py --api-key "$KEY" --measurement-ids "ids-file" --start-date "2023-05-11" --stop-date "2023-06-11" --outdir "/etc/peering/data/ripe-measurements/"
+#exec python3 fetch-measurements.py --api-key "$API_KEY" --measurement-ids "ids-file" --start-date "2023-05-11" --stop-date "2023-06-11" --outdir "/etc/peering/data/ripe-measurements/"
