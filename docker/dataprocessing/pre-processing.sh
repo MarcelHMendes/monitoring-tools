9128772fca1dee2c6131651ed55f535921b903d9
Marcel Mendes <marcelmendes@dcc.ufmg.br>
2023-08-25 21:49:35 -0300

    update pre-processing script

--- docker/dataprocessing/pre-processing.sh
+++ docker/dataprocessing/pre-processing.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -eu
+#trap remove intermediate files
 
-python3 traceroutes_ip2file.py
+python3 traceroutes_ip2file.py --ripedir "/etc/peering/monitor/data/ripe-measurements" --outdir ips_file
 
-# Perform space-to-comma replacement
-# Remove lines with more than 3 elements (comma-separated)
-# Add line number at the beginning of the line (we use as PK)
-sed  -e 's/\s\+/,/g' "$input_file" | awk -F, 'NF <= 3 {print}' | nl -w1 -s',' > "$output_file"
+# remove duplicates and sort IPs
+sort -u ips_file > ips_file_sorted
 
+netcat whois.cymru.com 43 < ips_file_sorted | sort -n > ips_mapped
+
+# remove duplicates and sort mapping
+sort -u ips_mapped > ips_mapped_sorted
+
+sed 's/ //g; s/|/,/g'  ips_mapped_sorted > ips_mapped.csv
 # import data to database
-sqlite3 my_database.db ".import --csv $output_file prefix_asn_mapping"
-
-#rm $input_file
+#sqlite3 my_database.db ".import --csv $output_file prefix_asn_mapping"
