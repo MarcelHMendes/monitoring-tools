ed6ba066fa21faf975bb98fe28818ecd87bcca3d
Marcel Mendes <marcelmendes@dcc.ufmg.br>
2023-08-26 22:40:47 -0300

    update files

--- docker/dataprocessing/db.py
+++ docker/dataprocessing/db.py
@@ -7,51 +7,44 @@ import re
 Base = declarative_base()
 
 
-class PrefixASNMapping(Base):
-    __tablename__ = "prefix_asn_mapping"
+class IPASNMapping(Base):
+    __tablename__ = "ip_asn_mapping"
 
     id = Column(Integer, primary_key=True)
-    prefix = Column(String, nullable=False)
-    subnet_mask = Column(String, nullable=False)
-    asn = Column(String, nullable=False)
+    asn = Column(Integer, nullable=False)
+    ip = Column(String, nullable=False)
+    as_name = Column(String, nullable=False)
 
 
 class MappingDB:
     def __init__(self, db_file):
         self.engine = create_engine(db_file, echo=True)
-        self.table = PrefixASNMapping()
+        self.table = IPASNMapping()
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
 
-    def insert_data(self, prefix, subnet_mask, asn):
+    def insert_data(self, ip, asn, as_name):
         session = self.Session()
-        new_data = PrefixASNMapping(prefix=prefix, subnet_mask=subnet_mask, asn=asn)
+        new_data = IPASNMapping(ip=ip, asn=asn, as_name=as_name)
         session.add(new_data)
         session.commit()
 
-    def delete_data(self, prefix):
-        prefix_mapping = self.query_data(prefix)
-        if prefix_mapping:
+    def delete_data(self, ip):
+        ip_mapping = self.query_data(ip)
+        if ip_mapping:
             session = self.Session()
-            session.delete(prefix_mapping)
+            session.delete(ip_mapping)
             session.commit()
             return True
         return False
 
-    def query_data(self, prefix, subnet_mask=None):
+    def query_data(self, ip):
         session = self.Session()
-        if subnet_mask:
-            result = session.query(PrefixASNMapping).filter(
-                PrefixASNMapping.prefix == prefix
-                and PrefixASNMapping.subnet_mask == subnet_mask
-            )
-        else:
-            result = session.query(PrefixASNMapping).filter(
-                PrefixASNMapping.prefix == prefix
-            )
+
+        result = session.query(IPASNMapping).filter(IPASNMapping.ip == ip)
 
         if result.count() != 0:
-            result = result.first().prefix()
+            result = result.first().ip()
         else:
             result = None
         session.commit()
@@ -60,10 +53,9 @@ class MappingDB:
 
 
 def main():
-    raw_file = "routeviews-rv2-20230820-1200.pfx2as"
     db_file = "sqlite:///my_database.db"
     db = MappingDB(db_file)
-    result = db.query_data(prefix="138.185.228.0")
+    # result = db.query_data(prefix="138.185.228.0")
     # db.print_query(result)
 
 
--- docker/dataprocessing/pre-processing.sh
+++ docker/dataprocessing/pre-processing.sh
@@ -14,8 +14,9 @@ netcat whois.cymru.com 43 < ips_file_sorted | sort -n > ips_mapped
 # remove duplicates and sort mapped ips
 sort -u ips_mapped > ips_mapped_sorted
 
-# remove spaces, add comma separation, add line number in the beggining of the row (We'll use as PK)
-sed 's/ //g; s/|/,/g'  ips_mapped_sorted |  nl -w1 -s',' > ips_mapped.csv
+# remove spaces and add comma separation, add line number in the beggining of the row (We'll use as PK),
+# remove extra information
+sed 's/ //g; s/|/,/g'  ips_mapped_sorted |  nl -w1 -s',' | cut -d',' -f1-4 > ips_mapped.csv
 
 # import data to database
-sqlite3 my_database.db ".import --csv ips_mapped.csv prefix_asn_mapping"
+sqlite3 my_database.db ".import --csv ips_mapped.csv ip_asn_mapping"
