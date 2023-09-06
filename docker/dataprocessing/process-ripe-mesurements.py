d259f4c0e42cdba9d1f83543999ea6501870991d
MarcelHMendes <marcelmendes@dcc.ufmg.br>
2023-09-05 21:45:44 -0300

    update

--- docker/dataprocessing/process-ripe-mesurements.py
+++ docker/dataprocessing/process-ripe-mesurements.py
@@ -1,9 +1,7 @@
 import json
-import db
 import os
 import argparse
 import logging
-import ipaddress
 import sys
 import radix
 import ip2as
@@ -16,10 +14,30 @@ def get_ripe_files_list(dir):
     return files
 
 
-def ip2asn_mapping(asdictdb, radixdb, traceroute_hops):
+def set_logging():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    root.addHandler(handler)
+
+
+def routeviews_db():
+    radix_tree = radix.Radix()
+    ip2asn = ip2as.IP2ASRadix(radix_tree)
+    current_path = pathlib.Path().absolute()
+    local = ip2asn.download_latest_caida_pfx2as(current_path)
+    ip2asn = ip2asn.from_caida_prefix2as(local)
+    return ip2asn
+
+
+def cymru_db(opts):
+    ip2asn = ip2as.IP2ASDict()
+    ip2asn = ip2asn.from_team_cymru_sqlite3(opts.db_file)
+    return ip2asn
+
+def ip2asn_mapping(radixdb, asdictdb, traceroute_hops):
     hops = []
-    cont_radix = 0
-    cont_asdict = 0
     for hop in traceroute_hops:
         result = hop.get("result", None)
         if not result:
@@ -28,16 +46,11 @@ def ip2asn_mapping(asdictdb, radixdb, traceroute_hops):
         if not ip_str:
             continue
         asn = radixdb.get(ip_str)
-        cont_radix += 1
+        # if radixdb doesn't resolve, we'll try asdictdb
         if not asn:
             asn = asdictdb.get(ip_str, None)
-            if asn:
-                cont_asdict += 1
         hops.append(asn)
-    print(cont_radix, cont_asdict)
-
     return hops
-
 
 def create_parser():
     desc = """Convert IP traceroutes to ASN traceroutes"""
@@ -60,7 +73,7 @@ def create_parser():
         "--outdir",
         dest="outdir",
         action="store",
-        default="ips_list",
+        default="parsed_traceroutes",
         required=False,
         help="Directory where the dump file will be located",
     )
@@ -68,43 +81,35 @@ def create_parser():
 
 
 def main():
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    root.addHandler(handler)
+    set_logging()
 
     parser = create_parser()
     opts = parser.parse_args()
-    fd_out = open(opts.outdir, "a")
 
-    radix_tree = radix.Radix()
-    ip2asn = ip2as.IP2ASRadix(radix_tree)
-    current_path = pathlib.Path().absolute()
-    local = ip2asn.download_latest_caida_pfx2as(current_path)
-    ip2asn = ip2asn.from_caida_prefix2as(local)
-
-    ip2asn_2 = ip2as.IP2ASDict()
-    ip2asn_2 = ip2asn_2.from_team_cymru_sqlite3(opts.db_file)
-
+    rv_ip2asn = routeviews_db()
+    cy_ip2asn = cymru_db(opts)
 
     for file in get_ripe_files_list(opts.ripedir):
         fd = open(os.path.join(opts.ripedir, file), "r")
         data = json.load(fd)
 
         parsed_data = []
-        print(file)
         for traceroute in data:
             parsed_traceroute = {}
 
             parsed_traceroute["src_addr"] = traceroute.get("src_addr", "*")
             parsed_traceroute["dst_addr"] = traceroute.get("dst_addr", "*")
             parsed_traceroute["endtime"] = traceroute.get("endtime", "*")
-            parsed_traceroute["result"] = ip2asn_mapping(ip2asn_2,
-                ip2asn, traceroute.get("result", None)
+
+            mapping = ip2asn_mapping(
+                rv_ip2asn, cy_ip2asn, traceroute.get("result", None)
             )
+            parsed_traceroute["result"] = mapping
 
             parsed_data.append(parsed_traceroute)
 
-    print(parsed_data[-10:-1])
+    with open(opts.outdir, "w") as fd_out:
+        json.dump(parsed_data, fd_out)
+
+
 main()
